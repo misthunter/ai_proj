c08107db8004ab7fef33248c99f82f7881fee19d
misthunter <misthunter@hotmail.sg>
2025-02-07 08:43:39 +0800

    added code parser

--- test_generator.py
+++ test_generator.py
@@ -3,28 +3,50 @@
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_community.vectorstores import FAISS  
-from langchain_community.embeddings import HuggingFaceEmbeddings  
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain.chains import RetrievalQA, StuffDocumentsChain
-#class test_generator:
-#    def __init__(self, ):
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import Language
+from langchain_core.runnables import RunnablePassthrough
 
 
-
-def parse_source():
-    return 0
-    
 def parse_pdf(file_path):
     loader = PDFPlumberLoader(file_path)  
     docs = loader.load()
     return (docs)
+    
+def parse_code(file_path):
+    code_doc = []
 
-def setup_ai_model(model_name, file_path):
-    embeddings = HuggingFaceEmbeddings()  
-    documents = parse_pdf(file_path)
-    vector_store = FAISS.from_documents(documents, embeddings)  
+    with open(file_path, "r") as f:
+        content = f.read()
+        doc = Document(page_content=content, metadata={"filename": file_path, "file_index": 0})
+        code_doc.append(doc)
+
+    text_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.C, chunk_size=1000, chunk_overlap=0
+    )
+    texts = text_splitter.split_documents(code_doc)
+
+    return (texts)
+
+def setup_ai_model(model_name, pdf_file_path=None, code_file_path=None):
+   
+    if pdf_file_path is not None:
+        documents = parse_pdf(pdf_file_path)
+        embeddings = HuggingFaceEmbeddings() 
+        vector_store = FAISS.from_documents(documents, embeddings)  
+
+    elif code_file_path is not None:
+        texts = parse_code(code_file_path)
+        embeddings = OllamaEmbeddings(model="llama2:7b")
+        vector_store = FAISS.from_documents(texts, embeddings) 
 
     # Connect retriever  
     retriever = vector_store.as_retriever(search_kwargs={"k": 3}) 
@@ -34,40 +56,55 @@ def setup_ai_model(model_name, file_path):
     
     # Craft the prompt template  
     prompt = """  
-    1. Use ONLY the context below.  
-    2. If unsure, say "I don’t know".  
-    3. Keep answers under 4 sentences.  
-
-    Context: {context}  
-
-    Question: {question}  
-
-    Answer:  
+    Generate C unit tests for the {question}:
+    
+    Context:
+    {context}
+    
+    Include tests for:
+    1. Normal expected inputs
+    2. Edge cases
+    3. Invalid inputs
+    Use cunit syntax.
     """  
-    QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt)
+    #QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt)
     
     # Chain 1: Generate answers  
-    llm_chain = LLMChain(llm=llm, prompt=QA_CHAIN_PROMPT)  
-
+    #llm_chain = LLMChain(llm=llm, prompt=QA_CHAIN_PROMPT)  
+    
     # Chain 2: Combine document chunks  
     document_prompt = PromptTemplate(  
-        template="Context:\ncontent:{page_content}\nsource:{source}",  
-        input_variables=["page_content", "source"]  
+        template=prompt,  
+        input_variables=["context", "question"]  
+        #template=prompt_RAG, input_variables=["context", "question"]
     )  
 
-    # Final RAG pipeline  
-    qa = RetrievalQA(  
-        combine_documents_chain=StuffDocumentsChain(  
-            llm_chain=llm_chain,  
-            document_prompt=document_prompt,
-            document_variable_name=document_variable_name
-        ),  
-        retriever=retriever  
+    #qa = RetrievalQA(  
+    #    combine_documents_chain=StuffDocumentsChain(  
+    #        llm_chain=llm_chain,  
+    #        document_prompt=document_prompt,
+    #        document_variable_name=document_variable_name
+    #    ),  
+    #    retriever=retriever  
+    #)
+    #qa = RetrievalQA.from_llm(
+    #    llm=llm, prompt=prompt, retriever=retriever
+    #)
+    qa_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | document_prompt
+        | llm
+        | StrOutputParser()
     )
     
-    response = qa({"query": "what is SDM?"})
+    #response = qa({"query": "the function rsu_client_copy_status_log"})
+    #rag_chain = {"document_variable_name": document_variable_name} | document_prompt | llm | StrOutputParser()
+    question = "the function rsu_client_copy_status_log"
+    response = qa_chain.invoke(question)
     print(response)
     
 #test_generator()
 #parse_pdf()    
-setup_ai_model('deepseek-coder:6.7b', 'sample_pdf\\rsu.pdf')
+#setup_ai_model('deepseek-coder:6.7b', 'sample_pdf\\rsu.pdf')
+
+setup_ai_model('deepseek-coder:6.7b', code_file_path='sample_code\\rsu_client.c')
